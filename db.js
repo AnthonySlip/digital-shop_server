bd7c6b36cf7301da89d2b16382882dc5818de27f
Anton <slipchi@MacBook-Air-Anton.local>
2023-09-30 01:10:38 +0300

    first commit

--- db.js
+++ db.js
@@ -1,19 +1,15 @@
-const pg = require('pg')
 const {Sequelize} = require('sequelize')
 
 
-// module.exports = new Sequelize(
-//     'verceldb',//process.env.DB_NAME,
-//     process.env.POSTGRES_USER,
-//     process.env.POSTGRES_PASSWORD,
-//     {
-//         dialect: 'postgres',
-//         host: process.env.POSTGRES_HOST,
-//         port: process.env.DB_PORT
-//     }
-// )
-module.exports = new Sequelize(process.env.POSTGRES_URL, {
-    dialectModule: pg
-})
+module.exports = new Sequelize(
+    'verceldb',//process.env.DB_NAME,
+    process.env.POSTGRES_USER,
+    process.env.POSTGRES_PASSWORD,
+    {
+        dialect: 'postgres',
+        host: process.env.POSTGRES_HOST,
+        port: process.env.DB_PORT
+    }
+)
 
 
